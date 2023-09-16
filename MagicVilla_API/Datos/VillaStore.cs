7f84abdc16e8e9e66954fbef16b95c2651160aee
wilmercorrea97 <wilmercorrea97@gmail.com>
2023-09-16 17:29:32 -0300

    DbContext - Inyeccion de Dependencia

---
+++ MagicVilla_API/Datos/VillaStore.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_API.Modelos.Dto;
+using System.Net.NetworkInformation;
+
+namespace MagicVilla_API.Datos
+{
+    public static class VillaStore
+    {
+        public static List<VillaDto> villaList = new List<VillaDto>
+        {
+            new VillaDto{Id=1,Nombre="Vista a la piscina",Ocupantes=3,MetrosCuadrados=50},
+            new VillaDto{Id=2,Nombre="vista a la playa",Ocupantes=4,MetrosCuadrados=80}
+        };
+        
+    }
+}
---
+++ MagicVilla_API/Migrations/20230916200236_AlimentarTablaVilla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MagicVillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlimentarTablaVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Detalle", "FechaActualizacion", "FechaCreacion", "ImageUrl", "MetrosCuadrados", "Nombre", "Ocupantes", "Tarifa" },
+                values: new object[,]
+                {
+                    { 1, "", "Detalle de la villa...", new DateTime(2023, 9, 16, 17, 2, 35, 987, DateTimeKind.Local).AddTicks(7399), new DateTime(2023, 9, 16, 17, 2, 35, 987, DateTimeKind.Local).AddTicks(7350), "", 50, "Villa Real", 5, 200.0 },
+                    { 2, "", "Detalle de la villa...", new DateTime(2023, 9, 16, 17, 2, 35, 987, DateTimeKind.Local).AddTicks(7407), new DateTime(2023, 9, 16, 17, 2, 35, 987, DateTimeKind.Local).AddTicks(7404), "", 40, "Premiun Vista a la Piscina", 4, 150.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
